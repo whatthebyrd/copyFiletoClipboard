b9565c352b7fb2fd400290b78cbf2f7ff6ce6505
JM <easyas3.14@gmail.com>
2021-06-15 22:12:45 -0500

    Initial commit, Imported

---
+++ copyF2CB.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# copyF2CB
+# Invoke by: copyF2CB <Filename>
+# Takes contents of file and places into clipboard. Uses pyperclip; see website for OS-specific installs: https://pypi.org/project/pyperclip/
+# Original Creation: 26JUL20
+# Released under Apache 2.0 License
+
+import os
+import sys
+import pyperclip
+
+if os.path.exists(sys.argv[1]):  # Check if path is valid
+    file = open(sys.argv[1], "r")  # Open file in read-only mode
+    file_cont = file.read()  # Place file contents into var
+    file.close()  # Close file
+    pyperclip.copy(file_cont)
+
+else:
+    print("Path invalid. Exiting.")
